7938dfc55e17f4d032e2b89d2d9d26601512d3f6
Joshua Odaga <34144362+odaga@users.noreply.github.com>
2020-12-04 09:08:43 +0300

    added order routes

--- routes/OrderRoute.js
+++ routes/OrderRoute.js
@@ -13,31 +13,110 @@ const Cart = require('../Models/Cart');
 
 
 //POST('/order')
-router.post('/:id', (req, res) => {
+//RETRIEVING ALL THE PRODUCT FROM THE DATABASE
+router.get("/", (req, res) => {
     try {
-        //getting seller id
-        const id = req.params.id;
+        Order.find()
+        .exec()
+        .then(products => {
+            console.log(products);
+            if (products.length >= 1) {
+                res.status(200).json(products);
+            }
+            else {
+                res.status(404).json({
+                    message: "No products were found"
+                });
+            }
+        })
+        .catch (err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    } catch (error) {
+        console.log(error);
+            res.status(500).json({
+                error: error.message
+            });
+    }   
+});
 
-        Seller.findOne(id)
-            .exec()
-            .then(seller => {
-                seller.Orders
+
+
+//ADDING A NEW PRODUCT ORDER TO THE DATABASE
+router.post("/", (req, res) => {
+    try {
+        const newOrder = new Order({
+            _id: new mongoose.Types.ObjectId(),
+            name: req.body.productName,
+            description: req.body.productDescription,
+            price: req.body.productPrice,
+            productCategory: req.body.productCategory,
+            productImage: req.body.productImageUrl,
+            approvalStatus: false,
+            ownerId: req.body.productOwnerId
+        });
+    
+        newOrder.save()
+            .then(product => {
+                res.status(201).json({
+                    message: "Product order submitted successfully",
+                    newProduct: product
+                });
             })
             .catch(error => {
-                console.log(error.message);
+                console.log(error);
                 res.status(500).json({
-                    message: error.message
-                })
-            })
-
-
+                    error: error,message,
+                    message: "could not submit product order"
+                });
+            });
     } catch (error) {
-        console.log(error.message);
         res.status(500).json({
-            message: error.message
+            error: error.message,
+            message: "internal server error"
         });
     }
 });
+
+
+
+//RETRIEVE ONE FARMER'S  PRODUCT ORDERS FROM THE DATABASE
+router.get("/:id",(req, res) => {
+    try {
+        const id = req.params.id;
+    Order.findById(id)
+        .where('ownerId').equals(id)
+        .exec()
+        .then(product => {
+            if (product) {
+                res.status(200).json(product);
+            }
+            else {
+                res.status(404).json({
+                    message: "No product found"
+                });
+            }
+        })
+        .catch (error => {
+            console.log(error);
+            res.status(500).json({
+                error: error
+            });
+        });
+
+    } catch (error) {
+        console.log(error);
+            res.status(500).json({
+                error: error
+            });
+    }
+});
+
+
+
 
 
 
