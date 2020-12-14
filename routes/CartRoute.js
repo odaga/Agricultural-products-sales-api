fc3a90da270db1f0527281f2bb85fe5ed1db035e
Joshua Odaga <34144362+odaga@users.noreply.github.com>
2020-12-14 20:17:19 +0300

    cleaned CartRoute.js

--- routes/CartRoute.js
+++ routes/CartRoute.js
@@ -6,10 +6,6 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended:true}));
 
-
-const Product = require('../Models/Product'); //Product Schema
-const Seller = require('../Models/Seller'); //Seller Schema
-const Order = require('../Models/Order'); //Order Schema
 const Cart = require('../Models/Cart');
 
 
