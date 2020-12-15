e8bcb7e2b6d516387a5b87893b1c013b2308e2fa
Joshua Odaga <34144362+odaga@users.noreply.github.com>
2020-12-15 04:29:34 +0300

    fixd loop commnet

--- routes/OrderRoute.js
+++ routes/OrderRoute.js
@@ -51,7 +51,7 @@ router.post("/", (req, res) => {
     for (let i = 0; i < orderList.length; i++) {
         
         Order.save(orderList[i]);
-        console.log("Saved one now");
+        console.log("Saved one no");
     }
 
     res.status(200).json({
